18bbb5c9ef7b13643c2cf2a928605a18da930c94
icechair <714005+icechair@users.noreply.github.com>
2024-12-01 08:15:09 +0100

    undotree, cmp extracted

--- .config/nvim/lua/icechair/remap.lua
+++ .config/nvim/lua/icechair/remap.lua
@@ -7,3 +7,8 @@ vim.keymap.set("n", "<leader>fq", vim.cmd.Ex)
 vim.keymap.set('n', '<leader>b', function()
     vim.o.list = not vim.o.list
 end, { desc = 'toggle whitespaces' })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
