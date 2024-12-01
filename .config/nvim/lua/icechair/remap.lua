f616c442c11fd9b857d620c7fb2e11fbd3ac465a
icechair <714005+icechair@users.noreply.github.com>
2024-12-01 08:15:08 +0100

    treesitter, telescope, which-key

--- .config/nvim/lua/icechair/remap.lua
+++ .config/nvim/lua/icechair/remap.lua
@@ -3,3 +3,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 -- open directory of active buffer
 vim.keymap.set("n", "<leader>fq", vim.cmd.Ex)
+
+vim.keymap.set('n', '<leader>b', function()
+    vim.o.list = not vim.o.list
+end, { desc = 'toggle whitespaces' })
